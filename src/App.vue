a08e21105bdbedc51df7cc94009f34f388d88141
ghassen94 <mallouli.ghassen@yahoo.fr>
2022-04-15 21:05:46 +0100

    mise en place du projet

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <DefaultButton btnText="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+  import HelloWorld from './components/HelloWorld.vue'
+  import DefaultButton from './components/DefaultButton.vue'
+  export default {
+    name: 'App',
+    components: {
+      HelloWorld,
+      DefaultButton
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+
+  .default-btn {
+    
+  }
+</style>
---
+++ src/components/DefaultButton.vue
@@ -0,0 +1,29 @@
+<template>
+<button >
+<i class="bi-alarm"></i>
+
+  {{ btnText }}
+  <svg class="bi bi-volume-mute-fill" aria-hidden="true" >
+  </svg>
+</button>
+</template>
+
+<script>
+  import playIcon from './logo.svg';
+  import clockIcon from './logo.svg';
+
+  export default {
+    name: 'DefaultButton',
+    props: {
+      btnStyle: String,
+      btnTextStyle: String,
+      btnText: String,
+      action: Function,
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
